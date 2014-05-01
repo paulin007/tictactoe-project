ed2cc6f1889d692275825596f02c374b1d52aac6
tommy3gobbe@gmail.com <tommy3gobbe@gmail.com@d3bb2022-aede-2588-2f88-2b6fe5e7c38f>
2014-05-01 19:56:34 +0000

    Test del metodo generaMossaNew

---
+++ Tris/src/tests/Tests12.java
@@ -0,0 +1,28 @@
+package tests;
+
+import tris.TabellaTris;
+
+import computerIntelligenza.DifficoltàCasuale;
+
+public class Tests12 {
+
+	public static void main(String[] args) {
+		TabellaTris tabellaTris = new TabellaTris();
+
+		DifficoltàCasuale casuale = new DifficoltàCasuale();
+		tabellaTris.creaTabella();
+		tabellaTris.getCaselle().get(0).setSimbolo("g");
+		tabellaTris.getCaselle().get(2).setSimbolo("g");
+		tabellaTris.getCaselle().get(4).setSimbolo("g");
+		
+		
+		casuale.generaMossaNew(tabellaTris);
+		casuale.generaMossaNew(tabellaTris);
+		casuale.generaMossaNew(tabellaTris);
+		
+		
+		for (int i = 0; i < tabellaTris.getCaselle().size(); i++) {
+			System.out.println(tabellaTris.getCaselle().get(i).getIDcasella()+" "+tabellaTris.getCaselle().get(i).getSimbolo());
+		}
+	}
+}
