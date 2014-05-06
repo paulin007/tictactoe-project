6dcb5f2f5df3111680e9d01279e7c3edfd4a2135
giacomobellazzi@gmail.com <giacomobellazzi@gmail.com@d3bb2022-aede-2588-2f88-2b6fe5e7c38f>
2014-05-06 20:27:23 +0000

    Sistemate le 3 difficoltà che ora sono perfettamente funzionanti

--- Tris/src/computerIntelligenza/DifficoltàMedia.java
+++ Tris/src/computerIntelligenza/DifficoltàMedia.java
@@ -1,5 +1,7 @@
 package computerIntelligenza;
 
+import java.util.Random;
+
 import tris.TabellaTris;
 
 public class DifficoltàMedia implements Difficoltà {
@@ -8,14 +10,26 @@ public class DifficoltàMedia implements Difficoltà {
 	
 	@Override
 	public int generaMossa(TabellaTris tabellaTris) {
-		int mossa = -1;
-		for (int i = 0; i < tabellaTris.getCaselle().size()-2; i++) {
-			if(tabellaTris.getCaselle().get(i).isVuota()&&tabellaTris.getCaselle().get(i+1).isVuota()){
-				tabellaTris.getCaselle().get(i).setSimbolo(simboloPC);
-				mossa = i;
-				break;
-			}
-		}
+		int mossa = 0;
+		if(tabellaTris.possibileScacco()){
+				mossa = tabellaTris.getMossa();
+				tabellaTris.getCaselle().get(mossa).setSimbolo(simboloPC);
+				}else{
+					Random random = new Random();
+					boolean finito = false;
+					int k = 0;
+					int numero = -1; 
+					
+					while(!finito&& k<9){
+						k++;
+						mossa = random.nextInt(9);
+						if(tabellaTris.getCaselle().get(mossa).isVuota()){
+							tabellaTris.getCaselle().get(mossa).setSimbolo(simboloPC);
+							finito = true;
+						}
+					}
+				}
+		
 		return mossa;
 	}
 }
