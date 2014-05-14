949df4df73572879dc905adf5112922aa6eff99a
giacomobellazzi@gmail.com <giacomobellazzi@gmail.com@d3bb2022-aede-2588-2f88-2b6fe5e7c38f>
2014-05-14 14:46:14 +0000

    Aggiunta classe Interprete di messagio di rete

---
+++ trunk/TrisJava/src/rete/InterpreteMessaggio.java
@@ -0,0 +1,46 @@
+package rete;
+
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class InterpreteMessaggio {
+	
+	private String tipoMessaggio;
+	private String statoPartita;
+	private String IDpartita;
+	private String ultimoGiocatore;
+	private ArrayList<String> caselle;
+	
+	public void interpreta(String messaggio){
+		StringTokenizer stringTokenizer = new StringTokenizer(messaggio);
+		tipoMessaggio = stringTokenizer.nextToken();
+		IDpartita = stringTokenizer.nextToken();
+		statoPartita = stringTokenizer.nextToken();
+		ultimoGiocatore = stringTokenizer.nextToken();
+		caselle = new ArrayList<>();
+		while(stringTokenizer.hasMoreTokens()){
+			caselle.add(stringTokenizer.nextToken());
+		}
+	}
+
+	public String getTipoMessaggio() {
+		return tipoMessaggio;
+	}
+
+	public String getStatoPartita() {
+		return statoPartita;
+	}
+
+	public String getIDpartita() {
+		return IDpartita;
+	}
+
+	public String getUltimoGiocatore() {
+		return ultimoGiocatore;
+	}
+
+	public ArrayList<String> getCaselle() {
+		return caselle;
+	}
+	
+}
