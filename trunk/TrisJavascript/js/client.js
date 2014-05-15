e98ce5b14bc07ab6aa0a63359c3884989220402d
vanzulli.marco@gmail.com <vanzulli.marco@gmail.com@d3bb2022-aede-2588-2f88-2b6fe5e7c38f>
2014-05-15 09:33:22 +0000

    Aggiunta prima versione del sito HTML/JavaScript

---
+++ trunk/TrisJavascript/js/client.js
@@ -0,0 +1,29 @@
+/**
+ * Questo file js si occupa di inviare un pacchetto al server Java all'indirizzo ws://localhost:45454
+ * 
+ * @author Marco Vanzulli
+ */
+
+var webSocket = new WebSocket("ws://localhost:45454/test");
+
+webSocket.onopen = function(message) {
+	processOpen(message);
+};
+
+function sendMessage() {
+	webSocket.send("prova");
+}
+
+webSocket.onclose = function(message) {
+	processClose(message);
+};
+
+function processOpen(message) {
+	alert("connessione al server");
+}
+
+function processClose(message) {
+	webSocket.send("client disconnesso");
+	alert("connessione al server");
+}
+
