f4a95120a8a013a1d101b592bebb000d63879e05
amedeo.santo@gmail.com <amedeo.santo@gmail.com@d3bb2022-aede-2588-2f88-2b6fe5e7c38f>
2014-06-05 12:13:28 +0000

    Prova di inserimento libreria di supporto per Android e Java

---
+++ trunk/src/managers/TurnManager.java
@@ -0,0 +1,40 @@
+package managers;
+
+/**
+ * Classe la cui responsabilità è quella di
+ * gestire i turni di gioco
+ * @author Santo Amedeo
+ *
+ */
+public class TurnManager implements ITurnManager {
+
+	private boolean myTurn;
+	private boolean connected;
+	
+	/* (non-Javadoc)
+	 * @see trisGUI.ITurnManager#setMyTurn(boolean)
+	 */
+	@Override
+	public void setMyTurn(boolean myTurn) {
+		this.myTurn = myTurn;
+	}
+	
+	/* (non-Javadoc)
+	 * @see trisGUI.ITurnManager#isMyTurn()
+	 */
+	@Override
+	public boolean isMyTurn() {
+		return myTurn;
+	}
+	
+	@Override
+	public void setConnected(boolean connected){
+		this.connected = connected;
+	}
+	
+	@Override
+	public boolean isConnected() {
+		return connected;
+	}
+	
+}
