21876b7bfba4caf45d9135786f0a65dddc7fe160
kokou.adjignon01@universitadipavia.it <kokou.adjignon01@universitadipavia.it@d3bb2022-aede-2588-2f88-2b6fe5e7c38f>
2014-06-07 18:32:27 +0000

    Aggiornato la classe DefaultSettings

--- trunk/GamesLibraries/src/managers/DefaultSettings.java
+++ trunk/GamesLibraries/src/managers/DefaultSettings.java
@@ -1,7 +1,19 @@
 package managers;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 /**
+ * Singleton
  * contiene i cammini di default di tutti gli immagini, i font e i suoni usati nel progetto
  * @author K. ADJIGNON
  */
@@ -9,35 +21,69 @@ import java.util.HashMap;
 public class DefaultSettings {
 	
 	private static DefaultSettings settings = new DefaultSettings();
-	private HashMap<String, String> pathImmagini = new HashMap<String, String>();
+	private HashMap<String, String> paths = new HashMap<String, String>();
 		
 	private DefaultSettings() {
 	
 	}
 	
+	/**
+	 * restituisce l'unica istanza possibile della classe
+	 * @return settings
+	 */
 	public static DefaultSettings getSettings() {
 		return settings;
 	}
 	
-	public void caricaMappa() {
+	
+	/**
+	 * Carica la mappa mettendo tutti i percorsi degli immagini, font e suoni
+	 */
+	public void loadMap() {
 		
-		pathImmagini.put("griglia", "/forza4Gui/Immagini/griglia.png");
-		pathImmagini.put("forza4verticale", "/forza4Gui/Immagini/Forza4verticale.png");
-		pathImmagini.put("pallaGialla", "/forza4Gui/Immagini/palla_gialla.png");
-		pathImmagini.put("pallaRossa", "/forza4Gui/Immagini/palla_rossa.png");
-		pathImmagini.put("croce", "/trisGui/Immagini/X.png");
-		pathImmagini.put("cerchio", "/trisGui/Immagini/O.png");
-		pathImmagini.put("logo", "/trisGui/Immagini/Logo.png");
-		pathImmagini.put("indicatore1", "Immagini/indicatore1.png");
-		pathImmagini.put("indicatore2", "Immagini/indicatore2.png");
-		pathImmagini.put("sfondo2", "Immagini/Sfondo2.png");
-		pathImmagini.put("akhenaton", "/grafica/Akhenaton.ttf");
-		pathImmagini.put("applausi", "/suoni/applausi.wav");
-		pathImmagini.put("nur", "/suoni/nur.wav");
+		File filexml = new File("percorsi.xml");
+		
+		try {
+			
+			DocumentBuilderFactory dbfactory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder dbuilder = dbfactory.newDocumentBuilder();
+			Document document = dbuilder.parse(filexml);
+			
+			NodeList list = document.getElementsByTagName("object");
+			
+			for (int i = 0; i < list.getLength(); i++) {
+				Element element = (Element)list.item(i);
+				String name = element.getAttribute("name");
+				String path = element.getElementsByTagName("path").item(0).getTextContent();
+				
+				put(name, path);
+			}
+			
+		} catch (ParserConfigurationException e) {
+			e.printStackTrace();
+		} catch (SAXException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 	
+	/**
+	 * inserisce il percorso di un immagine, font e suono nella mappa
+	 * @param name
+	 * @param path
+	 */
+	public void put(String name, String path) {
+		paths.put(name, path);
+	}
+	
+	/**
+	 * dato il nome di un immagine o suono o font, ritorna il suo percorso
+	 * @param key
+	 * @return path
+	 */
 	public String getPath(String key){
-		return pathImmagini.get(key);
+		return paths.get(key);
 	}
 	
 }
